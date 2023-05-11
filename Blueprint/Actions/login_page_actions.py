846aa4c3ea083ff988ad21e1f0a9907a2cf8424b
MartinJAA <serverus15>
2023-05-11 11:16:05 -0400

    FT: Add navigate to URL function and move URL_LOGIN from .env to login_locators

--- Blueprint/Actions/login_page_actions.py
+++ Blueprint/Actions/login_page_actions.py
@@ -15,13 +15,14 @@ class LoginPage:
     title_locator = TITLE_LOCATOR
     username_label_locator = USERNAME_LABEL_LOCATOR
     password_label_locator = PASSWORD_LABEL_LOCATOR
+    URL_LOGIN = URL
 
     def __new__(cls):
         """Creates a unique instance of the session. Returns the created instance of the class."""
         if not cls._instance:
             cls._instance = super(LoginPage, cls).__new__(cls)
             cls._instance.driver = getattr(webdriver, os.environ.get("BROWSER"))()
-            cls._instance.driver.get(os.environ.get("URL"))
+            navigate_to_url(cls._instance.driver, cls.URL_LOGIN)
         return cls._instance
 
     def get_header(self) -> WebElement:
@@ -55,6 +56,10 @@ class LoginPage:
     def click_in_sign_button(self):
         """Clicks the sign-in button element"""
         find_element_by_id(self.driver, self.sign_in_button_locator).click()
+
+    def navigate_to_given_url(self, url):
+        """Navigates to the given URL using the LoginPage instance"""
+        navigate_to_url(self.driver, url)
 
     def close_open_browser(self):
         """Closes the provided browser driver instance"""
--- Libraries/DataProcessors/browser.py
+++ Libraries/DataProcessors/browser.py
@@ -1,6 +1,14 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
+def navigate_to_url(driver: WebDriver, url: str):
+    """Navigates to the given URL"""
+    try:
+        return driver.get(url)
+    except Exception as e:
+        raise Exception("It is not possible to navigate to" + url)
+
+
 def close_browser(driver: WebDriver):
     """Closes the provided browser driver instance"""
     try:
