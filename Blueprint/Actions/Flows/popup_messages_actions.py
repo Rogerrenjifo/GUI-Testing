77e5121fb9dfb991406d3439b3b4d75695e7b673
MariE <marieug.mc@gmail.com>
2023-05-16 01:06:31 -0400

    popup action file modified

--- Blueprint/Actions/Flows/popup_messages_actions.py
+++ Blueprint/Actions/Flows/popup_messages_actions.py
@@ -4,10 +4,6 @@ from Blueprint.PageObject.Flows.popup_messages_objects import PopupMessagesObjec
 class PopUpMessages(PopupMessagesObjects):
     """This class represents the popup messages actions in flow page"""
     
-    def click_popup_messages(self):
-        """Clicks save popup messages"""
-        self.get_popup_messages().click()
-        
     def click_close_popup_message(self):
         """Clicks close popup messages"""
         self.get_close_popup_message().click()
