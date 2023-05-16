81aa010b782aabc2bef190ef1c36e6497507bcf4
Carolina <carolina.pilar.v@gmail.com>
2023-05-15 23:25:10 -0400

    Add mapping header from flow feature

---
+++ Blueprint/Actions/Flows/header_actions.py
@@ -0,0 +1,45 @@
+from Blueprint.PageObject.Flows.header_objects import Header
+
+class HeaderActions(Header):
+    """This class represents the actions in the header of Flows page"""
+    
+    def deploy_dropdown(self):
+        # TODO
+        pass
+
+    def click_delete_option(self):
+        # TODO
+        pass
+
+    def deploy_select_version(self):
+        # TODO
+        pass
+
+    def click_version(self):
+        # TODO
+        pass
+
+    def click_save_button(self):
+        # TODO
+        pass
+
+    def click_save_next_button(self):
+        # TODO
+        pass
+
+    def click_create_form_tab(self):
+        # TODO
+        pass
+    
+    def click_create_flow_tab(self):
+        # TODO
+        # get create flow tab
+        pass
+    
+    def click_permissions_tab(self):
+        # TODO
+        pass
+
+    def click_publish_tab(self):
+        # TODO
+        pass
