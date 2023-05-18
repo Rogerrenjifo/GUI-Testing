64721e1cdad7268f2641b3c9c1edd3438006b98e
celestepalet <111649534+celestepalet@users.noreply.github.com>
2023-05-18 11:36:05 -0300

    BG-95: Implement 'Create form' tab - Main panel (#14)
    * BG-95: Implement 'Create form' tab - Main panel
    
    * BG-95: improve actions
    
    * BG-95: changes requested
    
    * BG-95: second changes requested

---
+++ Blueprint/Actions/Flows/create_form_main_panel_actions.py
@@ -0,0 +1,98 @@
+import time
+from robot.api import logger
+from Blueprint.PageObject.Flows.create_form_main_panel_objects import FormMainPanelPage
+
+
+class FormMainPanelActions(FormMainPanelPage):
+    """Represents the main panel actions of create form tab in flow page"""
+
+    def select_component(self, component_id: str):
+        """Clicks a component from main panel for display its properties"""
+        self.get_component(component_id).click()
+
+    def select_section(self, section_title: str):
+        """Clicks a section from main panel for display its properties"""
+        self.get_section(section_title).click()
+
+    def move_component_between_sections(self, component_id: str, column_id: str):
+        """Moves a component from a section to other"""
+        source = self.get_component(component_id)
+        target = self.get_section_column(column_id)
+        self.action_chains.drag_and_drop_element(source, target)
+
+    def move_section_up_other(self, source_section_title: str, target_section_title: str,
+                              source_index=None, target_index=None):
+        """Moves a section above another section"""
+        source = self.get_section(source_section_title, source_index)
+        target = self.get_section(target_section_title, target_index)
+        self.action_chains.custom_drag_and_drop(source, target, direction="up")
+
+    def move_section_down_other(self, source_section_title: str, target_section_title: str,
+                                source_index=None, target_index=None):
+        """Moves a section below another section"""
+        source = self.get_section(source_section_title, source_index)
+        target = self.get_section(target_section_title, target_index)
+        self.action_chains.custom_drag_and_drop(source, target)
+
+    def move_component_up_other(self, source_component_id: str, target_component_id: str):
+        """Moves a component above another component"""
+        source = self.get_component(source_component_id)
+        target = self.get_component(target_component_id)
+        self.action_chains.custom_drag_and_drop(source, target, direction="up")
+
+    def move_component_down_other(self, source_component_id: str, target_component_id: str):
+        """Moves a component below another component"""
+        source = self.get_component(source_component_id)
+        target = self.get_component(target_component_id)
+        self.action_chains.custom_drag_and_drop(source, target)
+
+    def delete_section(self, section_title: str, index=None):
+        """Deletes a section from its drop-down menu"""
+        self.display_section_delete_menu(section_title, index)
+        self.select_section_delete_button()
+
+    def delete_component(self, component_id):
+        """Deletes a component from its drop-down menu"""
+        self.display_component_delete_menu(component_id)
+        self.select_component_delete_button(component_id)
+
+    def display_section_delete_menu(self, section_title: str, index=None):
+        """Clicks drop-down button, if it does not display, clicks on its corner"""
+        drop_down_button = self.get_section_dropdown(section_title, index)
+        self.action_chains.move_to_an_element(drop_down_button)
+        drop_down_button.click()
+        display_drop_down_menu = drop_down_button.get_attribute("aria-expanded")
+        if display_drop_down_menu.lower() == "false":
+            logger.info("Clicking on middle drop-down button failed, "
+                        "It will try clicking on one side of the button")
+            self.action_chains.custom_click_element(drop_down_button)
+        time.sleep(2)
+
+    def display_component_delete_menu(self, component_id: str):
+        """Displays component drop-down menu from its 3 dots button"""
+        self.action_chains.move_to_an_element(self.get_component_dropdown(component_id))
+        self.get_component_dropdown(component_id).click()
+        time.sleep(2)
+
+    def obtain_section_error_message_from_main_panel(self, section_title: str, index=None):
+        """Returns section error message displayed in main panel"""
+        message = self.get_section_error_message(section_title, index).text
+        return message
+
+    def obtain_section_title_from_main_panel(self, index: str):
+        """Returns section title displayed in main panel"""
+        section_title = self.get_section_title(index).text
+        return section_title
+
+    def obtain_component_title_from_main_panel(self, component_id: str):
+        """Returns component title displayed in main panel"""
+        component_title = self.get_component_title(component_id).text
+        return component_title
+
+    def select_section_delete_button(self):
+        """Clicks delete button from section drop-down menu"""
+        self.get_section_delete_button().click()
+
+    def select_component_delete_button(self, component_id: str):
+        """Clicks delete button from component drop-down menu"""
+        self.get_component_delete_button(component_id).click()
--- Libraries/Drivers/action_chains.py
+++ Libraries/Drivers/action_chains.py
@@ -1,4 +1,7 @@
 from selenium.webdriver import ActionChains
+from selenium.webdriver.remote.webelement import WebElement
+from robot.api import logger
+
 
 class ActionsChains(object):
     """A class that provides methods to use with element on a webpage."""
@@ -6,6 +9,25 @@ class ActionsChains(object):
         self.driver = driver
         self.action_chains = ActionChains(self.driver)
 
-    def drag_and_drop_element(self, source, target):
+    def drag_and_drop_element(self, source: WebElement, target: WebElement):
         """Drags and drops an element"""
         self.action_chains.click_and_hold(source).move_to_element(target).release(target).perform()
+
+    def move_to_an_element(self, source: WebElement):
+        self.action_chains.move_to_element(source).perform()
+
+    def custom_drag_and_drop(self, source: WebElement, target: WebElement, direction="down"):
+        """Drags and drops an element up or down other element"""
+        if direction.lower() == "down":
+            self.action_chains.click_and_hold(source).move_to_element(target).\
+                move_by_offset(0, 10).release().perform()
+        elif direction.lower() == "up":
+            self.action_chains.click_and_hold(source).move_to_element(target).\
+                move_by_offset(0, -10).release().perform()
+        else:
+            logger.info(f"The input '{direction}' is an invalid direction, "
+                        f"please send 'up' or 'down'")
+
+    def custom_click_element(self, source: WebElement):
+        """Clicks on a side of an element"""
+        self.action_chains.move_to_element(source).move_by_offset(0, 5).click().perform()
