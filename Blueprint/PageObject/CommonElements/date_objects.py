1791b37ed8b18fda51336e3ac82630284cc66bcd
Leonardo Pacheco <lpachecoc@unal.edu.co>
2023-05-22 13:52:02 -0500

    Changed locators use.

--- Blueprint/PageObject/CommonElements/date_objects.py
+++ Blueprint/PageObject/CommonElements/date_objects.py
@@ -7,32 +7,25 @@ class DateObjects(BasePage):
     
     def __init__(self, driver):
         super().__init__(driver)
-        self.__date_default_year_select = locators.DEFAULT_DATE_YEAR_SELECT_LOCATOR
-        self.__date_default_year_option = locators.DATE_DEFAULT_YEAR_OPTION_LOCATOR
-        self.__date_default_month_select = locators.DEFAULT_DATE_MONTH_SELECT_LOCATOR
-        self.__date_default_month_option = locators.DATE_DEFAULT_MONTH_OPTION_LOCATOR
-        self.__date_default_day_option = locators.DATE_DEFAULT_DAY_OPTION_LOCATOR
-        self.__prev_month_button = locators.PREV_MONTH_BUTTON_LOCATOR
-        self.__next_month_button = locators.NEXT_MONTH_BUTTON_LOCATOR
     
     def get_prev_month_button(self) -> WebElement:
         """Finds and returns previous month button."""
-        element = self.find_element.by_xpath(self.__prev_month_button)
+        element = self.find_element.by_xpath(locators.PREV_MONTH_BUTTON_LOCATOR)
         return element
     
     def get_next_month_button(self) -> WebElement:
         """Finds and returns nextious month button."""
-        element = self.find_element.by_xpath(self.__next_month_button)
+        element = self.find_element.by_xpath(locators.NEXT_MONTH_BUTTON_LOCATOR)
         return element
         
     def get_date_default_year_select(self) -> WebElement:
         """Finds and returns date default value year select."""
-        element = self.find_element.by_xpath(self.__date_default_year_select)
+        element = self.find_element.by_xpath(locators.DEFAULT_DATE_YEAR_SELECT_LOCATOR)
         return element
     
     def get_date_default_year_value(self, year: str) -> WebElement:
         """Finds and returns a year option from the dropdown with the required value."""
-        new_xpath = self.__date_default_year_option.replace("<<value>>", year)
+        new_xpath = locators.DATE_DEFAULT_YEAR_OPTION_LOCATOR.replace("<<value>>", year)
         try:
             element = self.find_element.by_xpath(new_xpath)
             return element
@@ -41,12 +34,12 @@ class DateObjects(BasePage):
         
     def get_date_default_month_select(self) -> WebElement:
         """Finds and returns date default value month select."""
-        element = self.find_element.by_xpath(self.__date_default_month_select)
+        element = self.find_element.by_xpath(locators.DEFAULT_DATE_MONTH_SELECT_LOCATOR)
         return element
     
     def get_date_default_month_value(self, month: str) -> WebElement:
         """Finds and returns a month value from the dropdown with the required value."""
-        new_xpath = self.__date_default_month_option.replace("<<value>>", month)
+        new_xpath = locators.DATE_DEFAULT_MONTH_OPTION_LOCATOR.replace("<<value>>", month)
         try:
             element = self.find_element.by_xpath(new_xpath)
             return element
@@ -55,7 +48,7 @@ class DateObjects(BasePage):
     
     def get_date_default_day_value(self, day: str) -> WebElement:
         """Finds and returns a day value from the dropdown with the required value."""
-        new_xpath = self.__date_default_day_option.replace("<<value>>", day)
+        new_xpath = locators.DATE_DEFAULT_DAY_OPTION_LOCATOR.replace("<<value>>", day)
         try:
             element = self.find_element.by_xpath(new_xpath)
             return element
