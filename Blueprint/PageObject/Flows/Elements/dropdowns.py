ee22d0db61291abf17465e9995eb3f1b12920f31
Fabian <cabrejoariasfabian@gmail.com>
2023-05-23 09:48:25 -0500

    change cast

--- Blueprint/PageObject/Flows/Elements/dropdowns.py
+++ Blueprint/PageObject/Flows/Elements/dropdowns.py
@@ -8,17 +8,17 @@ class Dropdownbox(BasePage):
     def __init__(self, driver, number: int):
         """Builds the class constructor"""
         super().__init__(driver)
-        self.number = number
+        self.number = str(number)
 
     def get_title(self) -> str:
         """Gets the title of the dropdown"""
-        xpath = locators.TITLE.replace("<<number>>", str(self.number))
+        xpath = locators.TITLE.replace("<<number>>", self.number)
         title = self.find_element.by_xpath(xpath).text
         return title
 
     def click_dropdown(self):
         """Clicks the dropdown"""
-        xpath = locators.TEXT_BOX.replace("<<number>>", str(self.number))
+        xpath = locators.TEXT_BOX.replace("<<number>>", self.number)
         self.find_element.by_xpath(xpath).click()
 
     def select_dropdown_user(self, user: str):
@@ -28,29 +28,29 @@ class Dropdownbox(BasePage):
 
     def type_name_user(self, name: str):
         """Types the name of a user in the dropdown """
-        xpath = locators.INPUT_TEXT_BOX.replace("<<number>>", str(self.number))
+        xpath = locators.INPUT_TEXT_BOX.replace("<<number>>", self.number)
         self.find_element.by_xpath(xpath).send_keys(name)
 
     def delete_typed_name(self):
         """Deletes the typed name of the user in the dropdown"""
-        xpath = locators.INPUT_TEXT_BOX.replace("<<number>>", str(self.number))
+        xpath = locators.INPUT_TEXT_BOX.replace("<<number>>", self.number)
         self.find_element.by_xpath(xpath).clear()
 
     def delete_all_users(self):
         """Deletes all selected users from the dropdown"""
-        xpath = locators.DELETE_ALL_USERS.replace("<<number>>", str(self.number))
+        xpath = locators.DELETE_ALL_USERS.replace("<<number>>", self.number)
         self.find_element.by_xpath(xpath).click()
 
     def message_empty(self) -> str:
         """Gets the message when dropdown textbox is empty"""
-        xpath = locators.EMPTY_MESSAGE.replace("<<number>>", str(self.number))
+        xpath = locators.EMPTY_MESSAGE.replace("<<number>>", self.number)
         message = self.find_element.by_xpath(xpath).text
         return message
 
     def delete_selected_user(self, name: str):
         """Deletes a specific user from dropdown"""
         try:
-            xpath = locators.DELETE_ONE_USER.replace("<<number>>", str(self.number)).replace("<<user>>", name)
+            xpath = locators.DELETE_ONE_USER.replace("<<number>>", self.number).replace("<<user>>", name)
             self.find_element.by_xpath(xpath).click()
         except Exception:
             logger.info(f" user '{name}' does not exist")
@@ -68,5 +68,5 @@ class Dropdownbox(BasePage):
 
     def click_drop_arrow(self):
         """Click on the dropdown arrow"""
-        xpath = locators.DROPDOWN_ARROW.replace("<<number>>", str(self.number))
+        xpath = locators.DROPDOWN_ARROW.replace("<<number>>", self.number)
         self.find_element.by_xpath(xpath).click()
