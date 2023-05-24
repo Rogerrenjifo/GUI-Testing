433c8cc1c7adbb10903fca99b4c3ee9c04c67152
DenisseC02 <117701502+DenisseC02@users.noreply.github.com>
2023-05-24 08:02:46 -0400

    BG-126 Implement Project tracing - Flow (#39)
    * Implementation permissions
    
    * locators
    
    * Permissions locators
    
    * Apply recommended improvements
    
    * Add deleted user, deleted users and scroll bar
    
    * Add data type
    
    * solve conflicts
    
    * Solve conflicts
    
    * Implement project tracing flow
    
    * Add class description
    
    * Delete __init__.py
    
    deleted unnecessary file
    
    * Driver
    
    * delete driver
    
    * Merge branch 'develop' of https://github.com/Dam24/GUI-Blueprint into feature/process_permission_dc
    
    * delete driver, modify two methods name and add varible type
    
    * solve conflicts
    
    * Move folders
    
    * Move folders

---
+++ Blueprint/Actions/Projects/ProjectTracing/flow_actions.py
@@ -0,0 +1,37 @@
+from Blueprint.PageObject.Projects.ProjectTracing.flow_objects import ProjectFlowObject
+
+
+class ProjectFlowActions(ProjectFlowObject):
+    """This class represents the Flow section of the Project Tracing"""
+
+    def get_project_flow_title(self) -> str:
+        """Finds and returns the title of the flow"""
+        title_project_flow = self.get_title_project_flow().text
+        return title_project_flow
+
+    def get_start_text(self) -> str:
+        """Finds and return the text of the start of the flow"""
+        start_text = self.get_start_element().text
+        return start_text
+
+    def get_final_step_text(self) -> str:
+        """Finds and returns the text of the final step of the flow"""
+        final_step_text = self.get_final_step().text
+        return final_step_text
+
+    def get_flow_step_text_by_order(self, number: int) -> str:
+        """Finds and returns the text of the steps of the flow"""
+        step_text = self.get_flow_step(number).text
+        return step_text
+
+    def get_action_text_by_order(self, number: int) -> str:
+        """Finds and returns the text of actions of the flow"""
+        action_text = self.get_flow_action(number).text
+        return action_text
+
+    def get_data_popup(self, number: int) -> str:
+        """Finds and returns the data of the popup of the actions"""
+        action = self.get_flow_action(number)
+        self.action_chains.move_to_an_element(action)
+        data = self.get_popup_action().text
+        return data
