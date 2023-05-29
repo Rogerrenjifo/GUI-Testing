6f49574ac0093cd6bf50eee12a9a040738e453cc
DenisseC02 <117701502+DenisseC02@users.noreply.github.com>
2023-05-29 12:02:07 -0400

    BG-216 Add assertions and folder structure (#53)
    * Add assertions and folder structure
    
    * Add suggestion in logger.info.list_ignore
    
    * solve conflicts

---
+++ Libraries/Assertions/assertions.py
@@ -0,0 +1,54 @@
+from robot.api import logger
+from assertpy import soft_assertions
+from assertpy import assert_that
+
+
+class Verification(object):
+    """A Verification class"""
+
+    def verify_equal_ignore(self, actual_result, expected_result, list_ignore=[]):
+        """Asserts the actual is equal to expected ignoring params"""
+        with soft_assertions():
+            logger.info("*****Expected******")
+            logger.info(expected_result)
+            logger.info("*****Actual******")
+            logger.info(actual_result)
+            logger.info(f"Are ignored: {list_ignore}")
+            assert_that(actual_result).is_equal_to(expected_result, ignore=list_ignore)
+
+    def verify_is_not_equal_ignore(self, actual_result, expected_result, list_ignore=[]):
+        """Asserts the actual is not equal to expected ignoring params"""
+        with soft_assertions():
+            logger.info("*****Expected******")
+            logger.info(expected_result)
+            logger.info("*****Actual******")
+            logger.info(actual_result)
+            logger.info(f"Are ignored: {list_ignore}")
+            assert_that(actual_result).is_not_equal_to(expected_result, ignore=list_ignore)
+
+    def verify_subset(self, expected_result, actual_result):
+        """Asserts the expected is subset of actual"""
+        with soft_assertions():
+            logger.info("*****Expected******")
+            logger.info(expected_result)
+            logger.info("*****Actual******")
+            logger.info(actual_result)
+            assert_that(expected_result).is_subset_of(actual_result)
+
+    def verify_a_dictionary_does_not_contain(self, expected_result, actual_result):
+        """Asserts the expected does not contain the dictionary of the actual"""
+        with soft_assertions():
+            logger.info("*****Expected******")
+            logger.info(expected_result)
+            logger.info("*****Actual******")
+            logger.info(actual_result)
+            assert_that(expected_result).does_not_contain_key(actual_result)
+
+    def verify_a_list_does_not_contain(self, expected_result, actual_result):
+        """Asserts the expected does not contain the list of actual"""
+        with soft_assertions():
+            logger.info("*****Expected******")
+            logger.info(expected_result)
+            logger.info("*****Actual******")
+            logger.info(actual_result)
+            assert_that(expected_result).does_not_contain(actual_result)
