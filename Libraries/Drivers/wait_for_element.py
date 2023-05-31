c9c16246eeaa990845a936ee5701fde2028f31d3
lpachecoc4 <36243170+lpachecoc4@users.noreply.github.com>
2023-05-31 09:42:57 -0500

    BG-215: Feature/refactor project tracing forms lp (#59)
    * Refactored code to work in different sections.
    
    * Corrected import.
    
    * Resolved comments made by Daniel.
    
    * Corrected suggestions made by Rocio.
    
    * Created new wait function with customized timeouts and xpaths,
    
    * Corrected comments from Roger and Damian.
    
    * Refactored actions and verifications for better coding practices.
    
    * Uncommented setup line.
    
    * Corrected typo

---
+++ Libraries/Drivers/wait_for_element.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+class WaitForElement(object):
+    """A class that provides methods to wait for elements to load on a webpage."""
+    def __init__(self, driver):
+        self.driver = driver
+    
+    def wait_for_element_with_xpath(self, xpath: str, timeout: int = 10):
+        """Waits (timeout) seconds for element with given xpath to load."""
+        wait = WebDriverWait(self.driver, timeout).until(lambda x: x.find_element(By.XPATH, xpath))
