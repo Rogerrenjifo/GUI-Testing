26cc08f2b1eb01d1665bb443f99b64d9dd68879f
Rocio Morales <roncio.93@gmail.com>
2023-06-07 15:28:37 -0300

    commit comment

--- Libraries/Drivers/browser.py
+++ Libraries/Drivers/browser.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from os import getenv
+from robot.api import logger
 
 
 class Browser(object):
@@ -56,6 +57,7 @@ class Browser(object):
             driver_class = browser_config.get("driver")
             options = browser_config.get("options")
             if bool(ci_execution):
+                logger.console("entró al ci execution")
                 options.add_argument("--headless")
                 options.add_argument("--no-sandbox")
         else:
